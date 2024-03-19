13991ba72a86057db45ae76adc2bcff5735fc637
Manish <neupanem01@gmail.com>
2024-03-19 11:36:48 +0545

    front end 1

--- EMart.DA/Repository/IRepository/ICategoryRepository.cs
+++ EMart.DA/Repository/IRepository/ICategoryRepository.cs
@@ -9,6 +9,7 @@ namespace EMart.DA.Repository.IRepository
 {
     public interface ICategoryRepository : IRepository<Category>
     {
+       
         void Update (Category category);
     }
 }
---
+++ EMart.Models/Models/ViewModels/ProductVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMart.Models.Models.ViewModels
+{
+    public class ProductVM
+    {
+       
+        public IEnumerable<Product> ProductList { get; set; }
+    }
+}
